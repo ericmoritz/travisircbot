6767dad14fe93e4e9da6d2b6861ff625ce671447
Eric Moritz <eric@themoritzfamily.com>
2012-08-24 10:19:37 -0400

    Made the failing event more annoying

--- arduino_src/passfail/passfail.ino
+++ arduino_src/passfail/passfail.ino
@@ -10,18 +10,24 @@ void setup() {
 }
 
 void loop() {
-    if(Serial.available() > 0) {
-       in_byte = Serial.read();
-       Serial.println(in_byte);
-       switch(in_byte) {
+   switch(in_byte) {
          case 'F': // Fail
-            digitalWrite(red_led, HIGH);
             digitalWrite(green_led, LOW);
+            digitalWrite(red_led, HIGH);
+            delay(250);
+            digitalWrite(red_led, LOW);
+            delay(250);
             break;
          case 'P':
             digitalWrite(green_led, HIGH);
             digitalWrite(red_led, LOW);
             break;
        }
+}
+
+void serialEvent() {
+    if(Serial.available() > 0) {
+       in_byte = Serial.read();
+       Serial.println(in_byte);   
     }
 }
